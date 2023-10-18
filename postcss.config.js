469ac4c7760679f0c00c287d2ce41b44b8fe4a06
HeeChan Kang <heech.kang@gmail.com>
2023-10-18 16:19:45 +0900

    fix: try to get css to work

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        ...(process.env.JEKYLL_ENV == 'production'
-        ? [require('cssnano')({ preset: 'default' })]
-        : [])
-    ]
-}
+  plugins: [
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.JEKYLL_ENV == "production"
+      ? [require("cssnano")({ preset: "default" })]
+      : []),
+  ],
+};
